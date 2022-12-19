53455522a8cc49c04731b6eace6b337c1ab51fdf
Fisatidi Andrey <fissatidi@gmail.ru>
2022-12-19 15:38:42 +0500

    4.1

--- src/main/java/ru/hogwarts/school/Repositories/StudentRepository.java
+++ src/main/java/ru/hogwarts/school/Repositories/StudentRepository.java
@@ -1,15 +1,25 @@
 package ru.hogwarts.school.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.hogwarts.school.model.Student;
 
 import java.util.Collection;
+import java.util.List;
 
- public interface StudentRepository extends JpaRepository<Student, Long> {
+public interface StudentRepository extends JpaRepository<Student, Long> {
     public Collection<Student> findByAge(int age);
+
     public Collection<Student> findByAgeBetween(int min, int max);
 
     public Collection<Student> findByFacultyId(Long facultyId);
 
+    @Query(value = "SELECT COUNT (*) FROM Student", nativeQuery = true)
+    Long getAmountOfAllStudents();
 
+    @Query(value = "SELECT AVG (age) FROM Student", nativeQuery = true)
+    Double getAverageAgeOfAllStudents();
+
+    @Query(value = "SELECT * FROM Student ORDER BY id DESC LIMIT 5", nativeQuery = true)
+    List<Student> getLastFiveStudents();
 }
